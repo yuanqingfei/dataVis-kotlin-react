8194b0a922874e30dd7c5b7bdd9cd78fe1befbdd
Aaron.Yuan <yuanqingfei.126.com>
2019-11-17 12:16:37 -0500

    cannot enhance anymore:(

--- src/app/ChartjsSortComponent.kt
+++ src/app/ChartjsSortComponent.kt
@@ -22,90 +22,58 @@ class ChartjsSortComponent: RComponent<RProps, RState>() {
     var chartReference: dynamic = js("{}")
 
     val initState: RState = js("""
-                        {
-                            labels: ['美国', '中国', '日本', '德国', '英国', '法国', '印度', '意大利', '巴西', '加拿大'],
-                            datasets: [{
-                              label: "2018",
-                              backgroundColor: [
-                                "#FF5733",
-                                "#884EA0",
-                                "#2471A3",
-                                "#17A589",
-                                "#D4AC0D",
-                                "#CA6F1E",
-                                "#BA4A00",
-                                "#F8C471",
-                                "#7FB3D5",
-                                "#C39BD3"
-                              ],
-                              data: [20494100.00, 13608151.86, 4970915.56, 3996759.29, 2825207.95, 2777535.24,
-                                2726322.62, 2073901.99, 1868626.09, 1712510.03]
-                            }]
-                        }
-                    """);
+        {
+            labels: ['美国', '中国', '日本', '德国', '英国', '法国', '印度', '意大利', '巴西', '加拿大'],
+            datasets: [{
+              label: "2018",
+              backgroundColor: [
+                "#FF5733",
+                "#884EA0",
+                "#2471A3",
+                "#17A589",
+                "#D4AC0D",
+                "#CA6F1E",
+                "#BA4A00",
+                "#F8C471",
+                "#7FB3D5",
+                "#C39BD3"
+              ],
+              data: [20494100.00, 13608151.86, 4970915.56, 3996759.29, 2825207.95, 2777535.24,
+                2726322.62, 2073901.99, 1868626.09, 1712510.03]
+            }]
+        }
+    """) as RState
+
     override fun componentDidMount() {
-//        Chart.defaults.global.animation?.duration = 3000
-//        console.log("outside update ");
-//        Chart.pluginService.register({
-//            fun beforeUpdate(chart: Chart) {
-//                var dataArray = chart.data.datasets?.get(0)?.data;
-//
-//                console.log("before update ");
-//
-//                var dataIndexes = dataArray.map{d, i -> i}
-//                dataIndexes.sort{a, b -> dataArray[a] - dataArray[b]}
-//                dataArray.sort{a, b -> a - b}
-//                var meta = chart.getDatasetMeta(0);
-//                var newMeta = emptyArray<MetaData>();
-//                var labels = chart.data.labels;
-//                var newLabels = emptyArray<String>();
-//                var newColors = emptyArray<String>();
-//
-//                meta.data.mapIndexed{i, bar -> {
-//                    var newIndex = dataIndexes.indexOf(i)
-//                    newMeta[newIndex] = bar
-//                    newLabels[newIndex] = chart.data.labels?.get(i)
-//                    newColors[newIndex] = chart.data.datasets?.get(0)?.backgroundColor[i]
-//                }};
-//
-//                meta.data = newMeta;
-//                chart.data.labels = newLabels;
-//                chart.data.datasets?.get(0)?.backgroundColor = newColors
-//            }
-//        })
-//        console.log(chartReference);
-
-        js("""
-            Chart.defaults.global.animation.duration = 3000;
-            Chart.pluginService.register({
-                beforeUpdate: function(chart) {
-                  var dataArray = chart.data.datasets[0].data;
-                    
-                    console.log("before update ");
-                    
-                  var dataIndexes = dataArray.map(function (d, i) {return i});
-                  dataIndexes.sort(function(a, b){
-                    return dataArray[a] - dataArray[b];
-                  });
-                  dataArray.sort(function(a, b){return a - b});
-                  var meta = chart.getDatasetMeta(0);
-                  var newMeta = [];
-                  var labels = chart.data.labels;
-                  var newLabels = [];
-                  var newColors = [];
-                  meta.data.forEach(function(bar, i) {
-                    var newIndex = dataIndexes.indexOf(i);
-                    newMeta[newIndex] = bar;
-                    newLabels[newIndex] = chart.data.labels[i];
-                    newColors[newIndex] = chart.data.datasets[0].backgroundColor[i];
-                  });
-                  meta.data = newMeta;
-                  chart.data.labels = newLabels;
-                  chart.data.datasets[0].backgroundColor = newColors;
+        Chart.defaults.global.animation?.duration = 10000
+        Chart.plugins.register(
+            js("""
+                {
+                    beforeUpdate: function(chart) {
+                      var dataArray = chart.data.datasets[0].data;
+                      var dataIndexes = dataArray.map(function (d, i) {return i});
+                      dataIndexes.sort(function(a, b){
+                        return dataArray[a] - dataArray[b];
+                      });
+                      dataArray.sort(function(a, b){return a - b});
+                      var meta = chart.getDatasetMeta(0);
+                      var newMeta = [];
+                      var labels = chart.data.labels;
+                      var newLabels = [];
+                      var newColors = [];
+                      meta.data.forEach(function(bar, i) {
+                        var newIndex = dataIndexes.indexOf(i);
+                        newMeta[newIndex] = bar;
+                        newLabels[newIndex] = chart.data.labels[i];
+                        newColors[newIndex] = chart.data.datasets[0].backgroundColor[i];
+                      });
+                      meta.data = newMeta;
+                      chart.data.labels = newLabels;
+                      chart.data.datasets[0].backgroundColor = newColors;
+                    }
                 }
-            })
-        """)
-
+            """)
+        )
         window.setInterval({chartReference.chartInstance.update()}, 3000)
     }
 
@@ -124,20 +92,22 @@ class ChartjsSortComponent: RComponent<RProps, RState>() {
                         reference:dynamic -> chartReference = reference
                     }
                     data = state
-                    options = js("""
-                                    {
-                                        scales: {
-                                          xAxes: [{
-                                            ticks: {
-                                              beginAtZero: true
+                    options =
+                            js("""
+                                        {
+                                            scales: {
+                                              xAxes: [{
+                                                ticks: {
+                                                  beginAtZero: true
+                                                }
+                                              }],
+                                              yAxes: [{
+                                                barThickness: 30
+                                              }]
                                             }
-                                          }],
-                                          yAxes: [{
-                                            barThickness: 30
-                                          }]
                                         }
-                                      }
-                                """) as? Chart.ChartOptions
+                                    """)
+                                    as Chart.ChartOptions
                 }
             }
         }
